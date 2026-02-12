66b2ece97a8a3551d5d43cf0d747b2d0bb74dd53
PASANTEIT <gustavoroalira@gmail.com>
2026-02-12 15:06:11 -0400

    Arreglo en database.py

--- backend-dashboard/app/main.py
+++ backend-dashboard/app/main.py
@@ -1,9 +1,10 @@
 # main.py para backend dashboard
 from fastapi import FastAPI
-from app.routes import publicidad
+from app.routes import publicidad, auth
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Dashboard Backend", version="1.0.0")
 
 app.include_router(publicidad.router)
+app.include_router(auth.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")

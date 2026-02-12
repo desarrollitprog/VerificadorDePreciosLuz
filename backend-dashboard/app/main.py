c0ea7d3a2dd729cdeeb819265363cbac8e673db0
PASANTEIT <gustavoroalira@gmail.com>
2026-02-12 10:20:26 -0400

    Cambios en la conexion con el backend, correccion de errores

--- backend-dashboard/app/main.py
+++ backend-dashboard/app/main.py
@@ -1,10 +1,9 @@
 # main.py para backend dashboard
 from fastapi import FastAPI
-from app.routes import consultas, publicidad
+from app.routes import publicidad
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Dashboard Backend", version="1.0.0")
 
-app.include_router(consultas.router)
 app.include_router(publicidad.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")

2dfddb13fff9284838bd2a4af1452a706d140628
PASANTEIT <gustavoroalira@gmail.com>
2026-02-23 10:14:37 -0400

    cambios en entrypoint

--- backend-api/entrypoint.sh
+++ backend-api/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 python app/create_tables_async.py
+# Eliminar carpeta si existe con el nombre del log y crear archivo vacío
+if [ -d "/app/heartbeat_errors.log" ]; then
+	rm -rf /app/heartbeat_errors.log
+fi
+touch /app/heartbeat_errors.log
 # Ejecutar heartbeat_client.py en segundo plano
 python heartbeat_client.py &
 # Iniciar la API principal

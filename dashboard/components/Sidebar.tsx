629b373a9c4e61b13a2a516b5b0f7758f113e4ef
PASANTEIT <gustavoroalira@gmail.com>
2026-02-23 08:33:06 -0400

    cambio en el sidebark para npm

--- dashboard/components/Sidebar.tsx
+++ dashboard/components/Sidebar.tsx
@@ -10,7 +10,7 @@ interface SidebarProps {
   onClose: () => void;
   onLogout: () => void;
 }
-
+const Sidebar: React.FC<SidebarProps> = ({ currentScreen, onNavigate, isOpen, onClose, onLogout }) => {
   const [role, setRole] = useState<'ADMIN' | 'CLIENTE' | null>(null);
 
   useEffect(() => {
@@ -104,4 +104,5 @@ interface SidebarProps {
       </div>
     </aside>
   );
-};
+}
+export default Sidebar;

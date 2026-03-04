47c3043bd052825d6d49b56d701e6a7779c73826
PASANTEIT <gustavoroalira@gmail.com>
2026-03-04 15:09:50 -0400

    Cambios para probar no ip

--- dashboard/services/axiosInstance.ts
+++ dashboard/services/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken, logout } from './authService';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || '/api',
 });
 
 api.interceptors.request.use((config) => {

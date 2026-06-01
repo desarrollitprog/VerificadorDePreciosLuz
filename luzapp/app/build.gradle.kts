dd66e7f1dbe23f55dc70a1e8ffb84c3428dc52a6
Tavorl25 <gustavoroalira@gmail.com>
2026-06-01 16:18:20 -0400

    Cambios para reinicio de escaner cada 30 seg 2

--- luzapp/app/build.gradle.kts
+++ luzapp/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         // Android 7.0 en adelante (API 24)
         minSdk = 24
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.7.0"
+        versionCode = 21
+        versionName = "2.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

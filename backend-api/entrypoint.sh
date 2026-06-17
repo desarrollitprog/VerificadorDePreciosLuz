5dc075b4079190fc57f9b19ea8a385546e211045
Tavorl25 <gustavoroalira@gmail.com>
2026-06-17 07:57:24 -0400

    Cambios para optimizacion de workers

--- backend-api/entrypoint.sh
+++ backend-api/entrypoint.sh
@@ -12,4 +12,4 @@ fi
 # Ejecutar heartbeat_client.py en segundo plano
 python heartbeat_client.py &
 # Iniciar la API principal
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
